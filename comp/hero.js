137498f8fb2be73c400e382c3d1c8532ad7a99a5
muhammadabrar <abrarmuhammad100@gmail.com>
2021-07-08 10:54:22 +0500

    tag-line

--- comp/hero.js
+++ comp/hero.js
@@ -5,11 +5,11 @@ export default function Hero(props) {
         <section class="home" id="home" style={{ backgroundImage: "url('./bg-2.jpg')" }}>
             <div class="max-width container">
                 <div class="home-content">
-                    <h1 className="hero-welcome">Hello again</h1>
+                    <h1 className="hero-welcome">Hello and Welcome</h1>
 
                     <a href="#timing" className="scroll-down-btn ">
                         <i className="fas fa-angle-double-down"></i>
-                    </a><p className="hero-welcome-note">It's time to welcome you back for pasta, pizza, vino. Finally!</p>
+                    </a><p className="hero-welcome-note">In The Home Of Taste In West Drayton</p>
                 </div>
 
             </div>
